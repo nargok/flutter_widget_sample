635abda6a6f63e658d42b891496b91e91eaa28d6
nargok <nargok@gmail.com>
2020-04-05 15:23:42 +0900

    DefaultTextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -7,42 +7,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: WidgetStudy()
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: WidgetStudy());
   }
 }
 
 class WidgetStudy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-     return Center(
-       child: Container(
-         child: Text.rich(
-           TextSpan(
-             text: 'Hello',
-             children: <TextSpan>[
-               TextSpan(text: 'beautiful',
-                        style: TextStyle(fontStyle: FontStyle.italic)),
-               TextSpan(text: 'world',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-             ],
-           )
-         ),
-       ),
-     );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Widget Study'),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 0.4,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          // ツリー下のTextStyleに同じものを適用
+          DefaultTextStyle(
+            style: Theme.of(context).textTheme.title,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[Text('A'), Text('B'), Text('C')],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[Text('E'), Text('F'), Text('G')],
+          )
+        ],
+      ),
+    );
   }
 }
